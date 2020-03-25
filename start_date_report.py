377d5e75ef01dea0b90a9c26fce2274e9d2f4630
tsm0420 <tsm0420@gmail.com>
2020-03-25 09:46:48 +0800

    Delete get_same_or_newer and combine func

--- start_date_report.py
+++ start_date_report.py
@@ -19,51 +19,30 @@ def get_start_date():
 
     return datetime.datetime(year, month, day)
 
-def get_same_or_newer(start_date):
+def list_newer(start_date):
     """Returns the employees that started on the given date, or the closest one."""
     file = open('employees-with-date.csv','w+')
     file.write(requests.get(FILE_URL, stream=True).content.decode("UTF-8"))
     data = file.readlines()
     file.close()
     reader = csv.reader(data[1:])
-    reader.sort(key = row[3])
 
     # We want all employees that started at the same date or the closest newer
     # date. To calculate that, we go through all the data and find the
     # employees that started on the smallest date that's equal or bigger than
     # the given start date.
-    min_date = datetime.datetime.today()
-    min_date_employees = []
-    for row in reader:
+
+    sorted_list = []
+
+    for item in reader:
+        sorted_list.append(item)
+        sorted_list.sort(key = sorted_list[3])
+
+    for row in sorted_list:
         row_date = datetime.datetime.strptime(row[3], '%Y-%m-%d')
 
-        # If this date is smaller than the one we're looking for,
-        # we skip this row
-        if row_date < start_date:
-            continue
-
-        # If this date is smaller than the current minimum,
-        # we pick it as the new minimum, resetting the list of
-        # employees at the minimal date.
-        if row_date < min_date:
-            min_date = row_date
-            min_date_employees = []
-
-        # If this date is the same as the current minimum,
-        # we add the employee in this row to the list of
-        # employees at the minimal date.
-        if row_date == min_date:
-            min_date_employees.append("{} {}".format(row[0], row[1]))
-
-    return min_date, min_date_employees
-
-def list_newer(start_date):
-    while start_date < datetime.datetime.today():
-        start_date, employees = get_same_or_newer(start_date)
-        print("Started on {}: {}".format(start_date.strftime("%b %d, %Y"), employees))
-
-        # Now move the date to the next one
-        start_date = start_date + datetime.timedelta(days=1)
+        if row_date >= start_date:
+            print("Started on {} : {} {}".format(row_date.strftime("%b %d, %Y"),row[0], row[1]))
 
 def main():
     start_date = get_start_date()
